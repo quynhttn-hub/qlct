1dc84c15828050b9f6acf9ecc19ded36e245adb7
quynhttn-hub <quynhnhu.tt1409@gmail.com>
2024-07-10 10:46:25 +0700

    fix bug

--- server/controllers/messageControllers.js
+++ server/controllers/messageControllers.js
@@ -83,6 +83,13 @@ const sendMessage = asyncHandler(async (req, res) => {
           const categoryAfter5Min = messageAfter5Min.category;
           const contentAfter5Min = messageAfter5Min.content;
 
+          if (
+            JSON.stringify(categoryAfter5Min) === "{}" ||
+            JSON.stringify(mentionAfter5Min) === "{}"
+          ) {
+            return;
+          }
+
           const sliceRemainingData = contentAfter5Min.slice(
             mentionAfter5Min.position,
             categoryAfter5Min.value.length + categoryAfter5Min.position + 1
@@ -90,7 +97,7 @@ const sendMessage = asyncHandler(async (req, res) => {
           const remainingData = contentAfter5Min
             .replace(sliceRemainingData, "")
             .trim();
-          
+
           if (
             !remainingData ||
             !mentionAfter5Min.value ||
@@ -109,7 +116,7 @@ const sendMessage = asyncHandler(async (req, res) => {
             .catch((error) => {
               console.log(error);
             });
-        }, 300000);
+        }, 10000); // sửa lại nha
 
         if (!category) {
           res.status(200).json({
@@ -193,7 +200,8 @@ const deleteMessage = asyncHandler(async (req, res) => {
 
 const updateMessage = asyncHandler(async (req, res) => {
   const { messageId } = req.params;
-  const { content, mention, category } = req.body;
+  const { content, mention, category, chatId } = req.body;
+  const chat = await Chat.findById(chatId);
 
   try {
     const message = await Message.findById(messageId);
@@ -214,7 +222,69 @@ const updateMessage = asyncHandler(async (req, res) => {
     message.mention = mention;
     message.category = category;
     await message.save();
-    res.status(200).json({ msg: "Message updated", message });
+    let messageUpdated = await Message.findById(messageId);
+    messageUpdated = await messageUpdated.populate("sender", "username avatar");
+    messageUpdated = await messageUpdated.populate("chat");
+
+    // check message
+    // if (mention) {
+    //   // nếu không có sheetId
+    //   if (!chat.sheetLink) {
+    //     res.status(200).json({
+    //       message,
+    //       msg: "Vui lòng tạo file quản lý để có thể ghi thông tin vào file",
+    //     });
+    //   } else {
+    //     if (!category) {
+    //       res.status(200).json({
+    //         message,
+    //         msg: "Nhập đầy đủ hạng mục chi tiêu",
+    //       });
+    //     } else {
+    //       const sliceRemaining = content.slice(
+    //         mention.position,
+    //         category.value.length + category.position + 1
+    //       );
+    //       const remain = content.replace(sliceRemaining, "").trim();
+    //       let money;
+    //       let note = "";
+    //       // Bước 2: Tách chuỗi thành hai phần dựa trên khoảng trắng đầu tiên
+
+    //       const firstSpaceIndex = remain.indexOf(" ");
+    //       if (firstSpaceIndex === -1) {
+    //         money = convertStringToNumber(remain);
+    //         note = "";
+    //       } else {
+    //         money = convertStringToNumber(remain.substring(0, firstSpaceIndex));
+    //         note = remain.substring(firstSpaceIndex + 1);
+    //       }
+
+    //       if (!money) {
+    //         res.status(200).json({
+    //           message,
+    //           msg: `Vui lòng nhập số tiền bạn ${mention.value} `,
+    //         });
+    //       } else {
+    //         const remaining = await readRemaining(chat.sheetLink);
+    //         if (mention.value == "chi tiêu" && remaining - money < 0) {
+    //           res.status(200).json({
+    //             message,
+    //             msg: "Bạn đã chi tiêu vượt quá giới hạn",
+    //           });
+    //         } else {
+    //           res.status(200).json({
+    //             message,
+    //             // msg: "Không đủ tiền để chi tiêu",
+    //           });
+    //         }
+    //       }
+    //     }
+    //   }
+
+    //   // }
+    // }
+
+    res.status(200).json({ msg: "Message updated", message: messageUpdated });
   } catch (err) {
     console.error(err.message);
     res.status(500).json({ msg: "Server error" });
